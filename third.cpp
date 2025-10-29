2feb42a8d6b5b159b7a17c8bbcecea174db57bbd
Dmytro Ananko <adymmka1234@gmail.com>
2025-10-29 14:06:32 +0000

    Add files via upload

---
+++ third.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cstdlib> 
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+if(argc<4){
+    return 0;
+}
+string command=argv[1];
+int a=atoi(argv[2]);
+int b=atoi(argv[3]);
+if(command=="mul"){
+    cout<<a/b<<endl;
+}
+return 0;
+}

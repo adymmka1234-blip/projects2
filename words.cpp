ba1925989c8e2d4f02d1ce6f3ab9301d8ae9d706
Dmytro Ananko <adymmka1234@gmail.com>
2025-12-01 20:36:21 +0000

    Add files via upload

---
+++ words.cpp
@@ -0,0 +1,48 @@
+#include <fstream>
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+
+int main(int argc, char* argv[]){
+   std::vector<std::string> v;
+     std::string sos;
+    std::ifstream in("searcher");
+    
+if(in.is_open()){
+ cout<<"файл открытий"<<endl;
+ while(getline(in,sos)){
+   v.push_back(sos);
+ 
+ 
+ 
+  
+ }
+
+ }
+ 
+int resultIndex=0;
+
+
+
+for(int i =1;i<v.size();i++){
+   if(v[i].size()<v[resultIndex].size()){
+    continue;
+   }
+   if( v[i].size() > v[resultIndex].size()){
+
+     resultIndex=i;
+
+   }
+  
+   
+}
+cout<<v[resultIndex]<<endl;
+
+
+in.close();
+}
+
+

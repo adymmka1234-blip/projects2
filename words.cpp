ae6b1c7e36f68965b6c70afc510c743076c6d271
Dmytro Ananko <adymmka1234@gmail.com>
2025-12-06 11:19:48 +0000

    Update words.cpp

--- words.cpp
+++ words.cpp
@@ -4,45 +4,42 @@
 #include <iostream>
 using namespace std;
 
-
-
-int main(int argc, char* argv[]){
-   std::vector<std::string> v;
-     std::string sos;
+int main(int argc, char* argv[]) {
+    std::vector<std::string> v;
+    std::string sos;
     std::ifstream in("searcher");
-    
-if(in.is_open()){
- cout<<"файл открытий"<<endl;
- while(getline(in,sos)){
-   v.push_back(sos);
- 
- 
- 
-  
- }
 
- }
- 
-int resultIndex=0;
+    if(in.is_open()){
+        cout << "файл открытий" << endl;
+        while(getline(in, sos)){
+            v.push_back(sos);
+        }
 
+        string word;
+        for(int i = 0; i < v.size(); i++){
+            while(getline(in, sos)){
+                for(char s : sos){
+                    if(s != ' '){
+                        word = s;
+                    }
+                    if(s == ' '){
+                        v.push_back(word);
+                    }
+                }
+            }
+        }
 
+        int resultIndex = 0;
+        for(int i = 1; i < v.size(); i++){
+            if(v[i].size() < v[resultIndex].size()){
+                continue;
+            }
+            if(v[i].size() > v[resultIndex].size()){
+                resultIndex = i;
+            }
+        }
+        cout << v[resultIndex] << endl;
 
-for(int i =1;i<v.size();i++){
-   if(v[i].size()<v[resultIndex].size()){
-    continue;
-   }
-   if( v[i].size() > v[resultIndex].size()){
-
-     resultIndex=i;
-
-   }
-  
-   
+        in.close();
+    }
 }
-cout<<v[resultIndex]<<endl;
-
-
-in.close();
-}
-
-

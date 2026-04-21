9dd1032c24e3deadcb59336b3a35851542bf6656
Dmytro Ananko <adymmka1234@gmail.com>
2026-04-21 19:21:23 +0000

    Add files via upload

--- sortWords.cpp
+++ sortWords.cpp
@@ -1,11 +1,17 @@
+
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <sstream>
 using namespace std;
 
-string reading (const string& fileName) {
+/*
+*reads and check is file open.
+*@return fileName which is shows his state
+*/
+string reading(const string& fileName) {
 ifstream in(fileName);
 if (!in.is_open() ) {
     cout << "File is not open" << endl;
@@ -13,44 +19,76 @@ if (!in.is_open() ) {
 return fileName;
 }
 
+/*
+*reads and also writes into vector result.
+* @return vector of words from the lines.
+*/
 vector <string> processData(const string& fileName) {
 string line;
 ifstream in(fileName);
 vector <string> result;
-while (getline(in, line)) {
-    result.push_back(line);
-}
-return result;
-
-}
-vector <string> SortWords(vector <string> lines ) {
-string word;
-int n = lines.size();
-for (int i =0; i<n-1;i++) {
-    for (int j=0;j< n - i - 1;j++) {
-        if (lines[j]>lines[j+1]){
-            swap (lines[j], lines[j+1]);
-        }
+    while (getline (in, line)) {
+        result.push_back(line);
     }
-   
+    return result;
 }
-return lines;
+
+
+
+/*
+*In first cicle after each iteration removes on one place
+*second cicle has the same idea but he is comparing first and the second to him word
+*if in first word has more letters and letters come first they change places and cicle will do it till the end 
+*/
+vector <string> sortWords(vector <string> lines ) {
+    vector <string> result;
+    for (string line : lines) {
+        istringstream iss(line);
+        vector<string> words;
+        string word;
+        
+       
+        while (iss >> word) {
+            words.push_back(word);
+        }
+        sort(words.begin(), words.end());
+
+        
+        string sortedLine = "";
+        for (int i = 0; i < words.size(); i++) {
+            sortedLine += words[i];
+            if (i != words.size() - 1) {
+                sortedLine += " ";
+            }
+        }
+
+        result.push_back(sortedLine);
+    }
+
+    return result;
 }
+
+
+/* 
+ *The function takes a variable named lines as an argument
+ * works with file name and writes the result into it
+ */
 void WriteData(const vector<string>& lines) {
     ofstream out("result");
-    for (string word: lines) {
-        out<<word<<endl;
-        cout<<word << ""<<endl;
+    for (string word: lines) {  
+        out << word << endl;
+        cout << word << "" << endl;
     }
 }
-int main (int argc,char**argv ) {
-    if (argc!= 2) {
-        cout << "Usage: " << argv[0] << " <filename>" << endl;
+int main(int argc,char**argv ) {
+    if (argc != 2) {
+        cout << "Usage:" << argv[0] << "<filename>" << endl;
         return 1;
-}
-string fileName=argv[1];
-string read = reading(fileName);
-vector <string> data=processData(read);
-vector <string> sort=SortWords(data);
-WriteData(sort);
+    }
+
+    string fileName= argv[1];
+    string read = reading(fileName);
+    vector <string> data = processData(read);
+    vector <string> sort = sortWords(data);
+    WriteData(sort);
 }
